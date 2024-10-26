719f056b0f9bf330a411d95ab9fa59e5dc1afc8b
Greve <frankgrevekrogh@gmail.com>
2024-10-26 12:48:12 +0200

    ox_lib menu option

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,23 +1,21 @@
--- Support Discord
--- https://discord.gg/zRCdhENsHG
-
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
+version '1.0.0'
 
 author 'TRClassic#0001'
 description 'Simple BlackMarket Script For QBCore'
-version '1.0.0'
 
 client_scripts {
     'client.lua'
 }
 
-shared_scripts {'Config.lua'}
+shared_scripts {
+    '@ox_lib/init.lua',
+    'Config.lua',
+}
 
 dependencies {
     'qb-menu',
     'qb-target'
 }
-
--- Support Discord
--- https://discord.gg/zRCdhENsHG
